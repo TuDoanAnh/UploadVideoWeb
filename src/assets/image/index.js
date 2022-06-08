caac7ce7c6d48c7e127db924e83948417a6b363f
Doan Anh Tu <doananhtu2507@gmail.com>
2022-06-08 22:10:54 +0700

    Update Image và thanh search của header

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,4 +1,5 @@
 const image = {
     logo: require('~/assets/image/logo-pokemon-79x45.png'),
+    avatar: require('~/assets/image/Pokemon/006.png'),
 };
 export default image;
--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,17 +2,18 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import image from '~/assets/image';
 
 const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img className={cx('avatar')} src="" alt="" />
+            <img className={cx('avatar')} src={image.avatar} alt="pokemon" />
             <div className="infor">
-                <p className={cx('name')}>
+                <h4 className={cx('name')}>
                     <span>Charizard</span>
-                    <FontAwesomeIcon icon={faCheckCircle} />
-                </p>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
                 <span className={cx('username')}>charizarddragon</span>
             </div>
         </div>
